66e145127e3fd10d54788cd235e167ad4e3bb917
Carlos Hernandez <carlos@devcodify.com>
2020-03-02 00:09:51 -0700

    create mainoptions function cms.js

--- cms.js
+++ cms.js
@@ -2,23 +2,24 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 const logo = require("asciiart-logo");
 
+
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "password",
-  database: "top_songsDB"
+    // Your password
+    password: process.env.MYSQL_PASSWORD,
+    database: "cms_DB"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
- // runSearch();
+connection.connect(function (err) {
+    if (err) throw err;
+    // runSearch();
 });
 
 
@@ -29,5 +30,38 @@ initialize();
 function initialize() {
     const logoText = logo({ name: "Primary C M S" }).render();
     console.log(logoText);
- 
+    mainOptions();
+}
+
+function mainOptions() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices:
+                [
+
+                    'View',
+                    'Add',
+                    'Delete',
+                    'Update'
+
+                ]
+        }])
+        .then(function (response) {
+            switch (response.action) {
+                case 'View':
+                     
+                case 'Add':
+                     
+                case 'Delete':
+                     
+                case 'Update':
+                     
+                default:
+                    console.log("Error: No option selected");
+            }
+        });
+
 }
