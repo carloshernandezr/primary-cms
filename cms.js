9945b4b0b434d2eddd4e020363254f6611d45569
Carlos Hernandez <carlos@devcodify.com>
2020-03-01 17:53:08 -0700

    create package.json, install npm library

---
+++ cms.js
@@ -0,0 +1,33 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+const logo = require("asciiart-logo");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "password",
+  database: "top_songsDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
+
+
+initialize();
+
+
+// Display logo text, load main prompts
+function initialize() {
+    const logoText = logo({ name: "Employee Manager" }).render();
+    console.log(logoText);
+    mainOptions();
+}
